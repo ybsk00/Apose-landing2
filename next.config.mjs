d00edfc0d6ae282bc72f2501850b4bacd8eb5d68
ybsk00 <ybsk00@naver.com>
2025-11-29 12:56:59 +0900

    Fix next.config.mjs syntax error

--- next.config.mjs
+++ next.config.mjs
@@ -6,13 +6,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-<<<<<<< HEAD
 }
 
 export default nextConfig
-=======
- 
-}
-
-export default nextConfig
->>>>>>> 2cbe64f01695bc19d4bfedebb0852d72a61feedb
